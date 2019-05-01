b225bd05e3352cafa6893e706284eeb40c20aa47
Amy Danielle Wilson <amydaniellewilson@gmail.com>
2019-05-01 20:25:29 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -7,23 +7,24 @@ var cards = [
 {
 	rank: 'queen',
 	suit:  'diamonds',
-	cardImage: "images/kings-of-hearts.png"
+	cardImage: "images/queen-of-diamonds.png"
 },
 {
 	rank: 'king',
 	suit:  'hearts',
-	cardImage: "images/kings-of-hearts.png"
+	cardImage: "images/king-of-hearts.png"
 },
 {
 	rank: 'king',
 	suit:  'diamonds',
-	cardImage: "images/kings-of-hearts.png"
+	cardImage: "images/king-of-diamonds.png"
 },
 ];
 
 var cardsInPlay = [];
 
 function checkForMatch() {
+	this.setAttribute('src',cards[cardId].cardImage);
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
 	} else {
@@ -31,11 +32,13 @@ function checkForMatch() {
 	}
 }
 
-function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId.rank]);
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId.rank]);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length===2) {
 	if (cardsInPlay[0]===cardsInPlay[2])
 		alert("You found a match!");
@@ -44,5 +47,15 @@ function flipCard(cardId) {
 		alert("Sorry, try again.");
 	}
 }
-flipCard(0);
-flipCard(2);
+
+function createBoard() {
+	for (var i  = 0; i < cards.length; i++) {	
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src','images/back.png');
+	cardElement.setAttribute('data-id',[i]);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
